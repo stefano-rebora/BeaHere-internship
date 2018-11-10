7bdca77a5981576e69a90c4d974c7848db395f8f
Andrea <andrea.canepa.1995@gmail.com>
2018-11-10 22:54:40 +0100

    Update database.js
    Added host for OSX

--- node_server/database.js
+++ node_server/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 //var exports = module.exports = {};
 
 module.exports.db = mysql.createPool({
-    host: "localhost",
+    host: "192.168.64.1", //for OSX, alternatively use localhost
     user: "root",
     connectionLimit: 100,
     password: "",
