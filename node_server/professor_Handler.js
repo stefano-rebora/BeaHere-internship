3fc64bb32a28adda3b084f631d46d7a95c5b8daf
Andrea <andrea.canepa.1995@gmail.com>
2018-11-10 23:01:05 +0100

    Update professor_Handler.js

--- node_server/professor_Handler.js
+++ node_server/professor_Handler.js
@@ -3,11 +3,11 @@ const bcrypt = require('bcrypt');
 //var exports = module.exports = {};
 
 module.exports.professorLogin = function (db, msg, res) {
-    console.log(msg.username);
-    console.log(msg.password);
+    //console.log(msg.id);
+    //console.log(msg.password);
 
-    if (msg.password.toString().trim() == "" || msg.username.toString().trim() == "") {
-        console.log("Empty username / password");
+    if (msg.password.toString().trim() == "" || msg.id.toString().trim() == "") {
+        console.log("Empty id / password");
         res.end(JSON.stringify({ result: false, err_msg: "Invalid Login", data: "" }));
         return;
     }
@@ -18,14 +18,15 @@ module.exports.professorLogin = function (db, msg, res) {
             return;
         }
 
-        console.log(hash);
+        //console.log(hash);
 
-        let query = "SELECT id FROM professor WHERE id = ? AND password = ?";
-        let attributes = [msg.username, hash];
+        let query = "SELECT * FROM professor WHERE id = ? AND password = ?";
+        let attributes = [msg.username, hash]; 
         db.query(query, attributes, (err, result, fields) => {
             console.log(query);
             console.log(attributes);
             if (err) {
+                console.log(err);
                 console.log("DB - Error");
                 res.end(JSON.stringify({ result: false, err_msg: "DB error", data: "" }));
             }
