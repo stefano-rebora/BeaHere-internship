861e6662c3535c0d3d8341c4b5be3ffb3e242548
Andrea <andrea.canepa.1995@gmail.com>
2018-11-11 20:24:17 +0100

    Update database.js

--- node_server/database.js
+++ node_server/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 //var exports = module.exports = {};
 
 module.exports.db = mysql.createPool({
-    host: "192.168.64.1", //for OSX, alternatively use localhost
+    host: "localhost", 
     user: "root",
     connectionLimit: 100,
     password: "",
