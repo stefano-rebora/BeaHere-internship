550bef69af55c8a28a2d04b0553905b892565aab
Andrea <andrea.canepa.1995@gmail.com>
2018-12-10 11:32:16 +0100

    Update professor_Handler

--- node_server/professor_Handler.js
+++ node_server/professor_Handler.js
@@ -2,40 +2,63 @@ const bcrypt = require('bcrypt');
 
 module.exports.professorLogin = function (db, msg, res) {
 
+    console.log(msg.id);
+    console.log(msg.password);
+
     if (msg.password.toString().trim() == "" || msg.id.toString().trim() == "") {
         console.log("Id and Password cannot be empty");
         res.end(JSON.stringify({ result: false, err_msg: "Invalid Login", data: "" }));
         return;
     }
 
-    bcrypt.hash(msg.password, 10, (err, hash) => {
+    //bcrypt.hash(msg.password, 10, (err, hash) => {
+
+      //  if (err) {
+        //    res.end(JSON.stringify({ result: false, err_msg: "Hash Error", data: "" }));
+          //  return;
+        //}
+
+    let query = "SELECT password FROM professor WHERE id = ?";
+        //let query = "INSERT INTO professor (id, name, surname, email, password) VALUES (?,?,?,?,?);"
+    let attributes = [msg.id];
+        //let attributes = [msg.id, "prova8", "prova8", "provamail", hash];
+
+    db.query(query, attributes, (err, result, fields) => {
+
+        console.log("query: " + query);
+        console.log("Attributes: " + attributes); 
+        console.log("result: " + result[0]);
+        console.log("fields: " + fields);
+
+        return;
+
         if (err) {
-            res.end(JSON.stringify({ result: false, err_msg: "Hash Error", data: "" }));
-            return;
+            console.log("DB - Error");
+            res.end(JSON.stringify({ result: false, err_msg: "DB Error", data: "" }));
         }
 
-        let query = "SELECT password FROM professor WHERE id = ?";
-        let attributes = [msg.id];
-        let stored_hash = '';
-        db.query(query, attributes, (err, result, fields) => {
+        stored_hash = result[0]['password'];
+
+        //if res vuoto....
+
+        bcrypt.compare(msg.password, stored_hash, function(err, res) {
+
             if (err) {
-                console.log("DB - Error");
-                res.end(JSON.stringify({ result: false, err_msg: "DB error", data: "" }));
+                res.end(JSON.stringify({ result: false, err_msg: "Hash Compare Error", data: "" }));
+                return;
             }
-            stored_hash = result[0]['password'];
-            console.log(stored_hash);
 
-            bcrypt.compare(msg.password, stored_hash, function(err, res) {
-                if (err) {
-                    res.end(JSON.stringify({ result: false, err_msg: "Hash Compare Error", data: "" }));
-                    return;
-                }
-                console.log(res); //se true ok login, e quindi...
-            });
-
+                //funzione per confronto con pwd generate in PHP
+/*                 bcrypt.compare(msg.password, stored_hash.replace(/^\$2y(.+)$/i, '\$2a$1'), function(err, result) {
+                    if (err) {
+                        res.end(JSON.stringify({ result: false, err_msg: "Hash Compare Error", data: "" }));
+                        return;
+                    } */
+            console.log(res);
         });
-
-
+                //console.log(res); //se true ok login, e quindi...
     });
+   // });
+    //console.log(res);
 
 }
